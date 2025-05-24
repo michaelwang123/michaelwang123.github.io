a83c77c43ae809f42a91f77b1eab7f70460a6990
michaelwang123 <286790238@qq.com>
2025-05-24 20:35:09 +0800

    1

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ tar -C /usr/local -xzf go1.22.1.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 # 安装 Hugo
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.123.8/hugo_extended_0.123.8_linux-amd64.tar.gz -o hugo.tar.gz
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.146.0/hugo_extended_0.146.0_linux-amd64.tar.gz -o hugo.tar.gz
 tar -xzf hugo.tar.gz
 mv hugo /usr/local/bin/hugo
 export PATH=/usr/local/bin:$PATH
